267e5879e7221ab65179f1d633d186270c5b6dba
ImDebabrata <debabratakgt@hotmail.com>
2023-01-22 02:52:17 +0530

    added drag functionallity

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { Box, Text, Stack, Icon } from "@chakra-ui/react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { MdDelete } from "react-icons/md";
 
-const TodoList = ({ props: { item, type } }) => {
+const TodoList = ({ props: { item, type, index } }) => {
   return (
-    <Box m={2} p={1} bgColor={`${type}.500`}>
-      <Stack
-        alignItems={"center"}
-        justifyContent={"space-between"}
-        direction={"row"}
-      >
-        <Text textAlign={"left"} m={"5px 0"} color={"white"}>
-          {item.title}
-        </Text>
-        <Icon as={MdDelete} boxSize={6} />
-      </Stack>
-    </Box>
+    <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
+      {(provided) => (
+        <Box
+          m={2}
+          p={1}
+          bgColor={`${type}.500`}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          props={{ item, type: "red" }}
+        >
+          <Stack
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            direction={"row"}
+          >
+            <Text textAlign={"left"} m={"5px 0"} color={"white"}>
+              {item.title}
+            </Text>
+            <Icon as={MdDelete} boxSize={6} />
+          </Stack>
+        </Box>
+      )}
+    </Draggable>
   );
 };
 
--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Heading, Flex } from "@chakra-ui/react";
 import TodoList from "../Components/TodoList";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const todos = [
-  { id: 1, title: "Dummy1" },
+  { id: "sou", title: "Dummy1" },
   { id: 2, title: "Dummy2" },
   { id: 3, title: "Dummy3" },
   { id: 4, title: "Dummy4" },
@@ -14,23 +15,50 @@ const todos = [
 ];
 
 const Todos = () => {
+  const [characters, updateCharacters] = useState(todos);
+
+  const handleOnDragEnd = (res) => {
+    const items = Array.from(characters);
+    const [reorderItem] = items.splice(res.source.index, 1);
+    items.splice(res.destination.index, 0, reorderItem);
+    updateCharacters(items);
+    // console.log("reorderItem:", reorderItem);
+  };
   return (
     <Box>
       <Heading m={"20px 0"} size={"lg"}>
         Todo
       </Heading>
       <Flex justifyContent={"space-around"}>
-        <Box w={"40%"} border={"1px solid black"}>
-          <Heading size={"md"}>Important</Heading>
-          {todos.map((item) => {
-            return <TodoList key={item.id} props={{ item, type: "red" }} />;
-          })}
-        </Box>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <Box
+                w={"40%"}
+                border={"1px solid black"}
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                <Heading size={"md"}>Important</Heading>
+                {characters.map((item, index) => {
+                  return (
+                    <TodoList
+                      key={item.id}
+                      props={{ item, type: "red", index }}
+                    />
+                  );
+                })}
+                {provided.placeholder}
+              </Box>
+            )}
+          </Droppable>
+        </DragDropContext>
+
         <Box w={"40%"} border={"1px solid black"}>
           <Heading size={"md"}>Low</Heading>
-          {todos.map((item) => {
+          {/* {todos.map((item) => {
             return <TodoList key={item.id} props={{ item, type: "green" }} />;
-          })}
+          })} */}
         </Box>
       </Flex>
     </Box>
